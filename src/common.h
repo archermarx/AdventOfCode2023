28f3607578d781ccfd7b288ce4caef219e6f96f1
Thomas Marks <thomasarchermarks@gmail.com>
2023-12-02 23:47:51 -0500

    day 2 done!

--- src/common.h
+++ src/common.h
@@ -11,5 +11,45 @@
 using std::cout;
 using std::string;
 using std::vector;
+using std::tuple;
+
+// STRING UTILITY FUNCTIONS
+
+const char* ws = " \t\n\r\f\v";
+
+// trim from end of string (right)
+inline std::string& rtrim(string& str, const char* t = ws){
+    str.erase(str.find_last_not_of(t) + 1);
+    return str;
+}
+
+// trim from beginning of string (left)
+inline string& ltrim(string& str, const char* t = ws){
+    str.erase(0, str.find_first_not_of(t));
+    return str;
+}
+
+// trim from both ends of string (right then left)
+inline string& trim(string& str, const char* t = ws){
+    return ltrim(rtrim(str, t), t);
+}
+// split string by delimiter
+vector<string> split(string str, string dlm) {
+    vector<string> splitstr;
+
+    size_t last = 0;
+    size_t next = 0;
+    string token;
+    while ((next = str.find(dlm, last)) != string::npos) {
+        token = str.substr(last, next - last);
+        splitstr.push_back(token);
+        last = next + 1;
+    }
+
+    token = str.substr(last);
+    splitstr.push_back(token);
+
+    return splitstr;
+}
 
 #endif
--- src/day02.cpp
+++ src/day02.cpp
@@ -1,9 +1,134 @@
 #include "common.h"
+#include <numeric>
+
+const string exampleInput1 = R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)";
+
+const vector<int> exampleGames1{1,2,5};
+
+const std::map<string, int> maxCubes1{
+    {"red", 12},
+    {"green", 13},
+    {"blue", 14}
+};
+
+const int exampleAnswer1 = std::reduce(exampleGames1.begin(), exampleGames1.end());
+const int exampleAnswer2 = 2286;
+
+bool validDraw(string draw) {
+    auto splitStr = split(draw, ", ");
+    for (auto &s : splitStr ) {
+        auto parts = split(trim(s), " ");
+        auto key = parts[1];
+        auto val = stoi(parts[0]);
+        if (val > maxCubes1.at(key)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool validGame(string line) {
+    auto draws = split(split(line, ": ")[1], ";");
+    for (auto &draw : draws) {
+        if (!validDraw(draw)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::map<string, int> parseDraw(string draw) {
+    auto ans = std::map<string, int>{{"red", 1}, {"green", 1}, {"blue", 1}};
+    auto splitStr = split(draw, ", ");
+    for (auto &s : splitStr ) {
+        auto parts = split(trim(s), " ");
+        auto key = parts[1];
+        auto val = stoi(parts[0]);
+        ans.at(key) =  val;
+    }
+    return ans;
+}
+
+int gamePower(string game) {
+    int maxRed = 1;
+    int maxGreen = 1;
+    int maxBlue = 1;
+    auto draws = split(split(game, ": ")[1], ";");
+    for (auto &draw : draws) {
+        auto parsed = parseDraw(draw);
+        maxRed = std::max(parsed.at("red"), maxRed);
+        maxGreen = std::max(parsed.at("green"), maxGreen);
+        maxBlue = std::max(parsed.at("blue"), maxBlue);
+    }
+    return maxRed * maxGreen * maxBlue;
+}
+
+template <typename T>
+void solution_01_1(T& stream, bool verbose) {
+    std::string line;
+    int sum = 0;
+    int ind = 1;
+    while (std::getline(stream, line)) {
+        std::istringstream lineStream(line);
+        bool isValid = validGame(line);
+        if (verbose) {
+            cout << "Game " << ind << ": " << isValid << "\n";
+        }
+        if (isValid) {
+            sum += ind;
+        }
+        ind += 1;
+    }
+
+    cout << "Solution: " << sum << "\n";
+}
+
+template <typename T>
+void solution_01_2(T& stream, bool verbose) {
+    std::string line;
+    int sum = 0;
+    while (std::getline(stream, line)) {
+        std::istringstream lineStream(line);
+        int power = gamePower(line);
+        if (verbose) {
+            cout << "Power = " << power << "\n";
+        }
+        sum += power;
+    }
+    cout << "Solution: " << sum << "\n";
+}
+
 
 int main() {
     string inputPath = "../input/day02.txt";
     
     cout << "Advent of code day 2!" << "\n";
     cout << "====================================" << "\n";
+
+    cout << "Part 1 example (expected " << exampleAnswer1 << ")\n";
+    std::istringstream example01(exampleInput1);
+    solution_01_1(example01, true);    
     
+    cout << "\nPart 1 solution" << "\n";
+    std::ifstream inputLines(inputPath);
+    solution_01_1(inputLines, false);
+
+    cout << "====================================" << "\n";
+
+    cout << "Part 2 example (expected " << exampleAnswer2 << ")\n";
+    std::istringstream example02(exampleInput1);
+    solution_01_2(example02, true);    
+    
+    cout << "\nPart 2 solution" << "\n";
+    std::ifstream inputLines2(inputPath);
+    solution_01_2(inputLines2, false);
+
+    //auto str = "Game 52: 14 blue, 3 red; 11 green, 6 red, 9 blue; 6 blue, 10 red; 1 red, 1 green, 4 blue; 9 blue, 6 green; 3 red, 2 blue, 8 green";
+    //cout << str << ": " << validGame(str) << "\n";
+
+    return 0;
 }
